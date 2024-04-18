e45d93a62becfb265ccfa50eb0ab06a8900035e5
carlosaalvrz <166819064+carlosaalvrz@users.noreply.github.com>
2024-04-18 15:06:36 +0200

    Foodslider php create

---
+++ foodslider.php
@@ -0,0 +1,43 @@
+<?php
+
+    require_once __DIR__ .'/bootstrap.php';
+    
+    $categories =[
+        [
+            'id' => 1,
+            'name' => 'Breakfast',
+            'image' => 'assets/images/breakfast.jpg',
+        ],
+        [
+            'id' => 2,
+            'name' => 'Sushi',
+            'image' => 'assets/images/sushi.jpg',
+        ],
+        [
+            'id' => 3,
+            'name' => 'Pizza',
+            'image' => 'assets/images/pizza.jpg',
+        ],
+        [
+            'id' => 4,
+            'name' => 'Burgers',
+            'image' => 'assets/images/burger.jpg',
+        ],
+        [
+            'id' => 5,
+            'name' => 'Pasta',
+            'image' => 'assets/images/pasta.jpg',
+        ],
+        [
+            'id' => 6,
+            'name' => 'Spanish',
+            'image' => 'assets/images/spanish.jpg',
+        ]
+        ,[
+            'id' => 7,
+            'name' => 'Desserts',
+            'image' => 'assets/images/desserts.jpg',
+        ],
+
+    ]
+?>
