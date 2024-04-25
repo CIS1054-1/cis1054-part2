2aaaf5d0154994a08a2929294e49003d516c2b2f
carlosaalvrz <166819064+carlosaalvrz@users.noreply.github.com>
2024-04-25 17:40:46 +0200

    about.php updated

--- about.php
+++ about.php
@@ -1,5 +1,4 @@
 <?php
-    require_once __DIR__ ."/dabase.php";
-
-    $db = new Db();
-    
+    require_once __DIR__ ."/bootstrap.php";
+    require_once __DIR__ ."/navbar.php";
+    echo $twig->render('about.html' , ['navbar_data' => $navbar_data]);
