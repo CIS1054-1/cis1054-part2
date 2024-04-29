509c7c3655870222cf9f202b8a2247a0405c8392
Gioele Giunta <giunta.gioele3@gmail.com>
2024-04-29 23:27:53 +0200

    Code until now commented

--- assets/js/genColor.js
+++ assets/js/genColor.js
@@ -1,5 +1,5 @@
 /** 
- *  * This function return a color from a sum that is a string
+    * genColor 
     *
     * @author Gioele Giunta
     * @version 1.3
@@ -7,8 +7,16 @@
     * @info Me (Gioele) am going to use the CAMEL CASE for the java files
 **/
 
+/**
+ *
+ * This function return a color by summarizing all the ASCII charcode together
+ * @param character 
+ * @returns color 
+ */
+
 function genColor(character) {
     let sum = 0;
+    //Summarize all the ASCII charcode together to obtain the sum of the ASCIIs
     for (let j = 0; j < character.length; j++) {
         let code = character.charCodeAt(j);
 
@@ -19,12 +27,15 @@ function genColor(character) {
         }
     }
 
+    //use module to transform sum in the range 0-255 for red, green, blue
     let r = ((sum * 14) % 256);
     let g = ((sum * 27) % 256);
     let b = ((sum * 47) % 256);
 
 
+    //Packs the red, green, and blue color components into a single 32 bit hexadecimal color value.
     let color = ((r << 16) | (g << 8) | b).toString(16);
+    //Check if the length is at least 6 otherwise full with 0 (darker)
     while (color.length < 6) {
         color = '0' + color;
     }
