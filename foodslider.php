7b98ecc5817853f0f6ecd14b57c3e2ce95e5339e
carlosaalvrz <alvarezmartinezcarlos03@gmail.com>
2024-05-07 17:46:36 +0200

    FoodSlider-Item-Photos of the menu updated

--- foodslider.php
+++ foodslider.php
@@ -1,39 +1,12 @@
 <?php
 
     require_once __DIR__ .'/bootstrap.php';
-    
-    $categories =[
-        [
-            'id' => 1,
-            'name' => 'Chicken',
-            'image' => 'chicken.png',
-        ],
-        [
-            'id' => 2,
-            'name' => 'Pizza',
-            'image' => 'pizza.png',
-        ],
-        [
-            'id' => 3,
-            'name' => 'Burgers',
-            'image' => 'burgers.png',
-        ],
-        [
-            'id' => 4,
-            'name' => 'Pasta',
-            'image' => 'pasta.png',
-        ],
-        [
-            'id' => 5,
-            'name' => 'Drinks',
-            'image' => 'drinks.png',
-        ]
-        ,[
-            'id' => 6,
-            'name' => 'Desserts',
-            'image' => 'desserts.png',
-        ],
 
-    ]
+    $result = $db->query("SELECT * from categories");
 
+    if($result &&  $result->num_rows > 0){
+        $categories = $result;
+    }else{
+        echo $twig->render('404.html');
+    } 
 ?>
--- item.php
+++ item.php
@@ -4,64 +4,18 @@
     require_once __DIR__ . '/navbar.php';
     require_once __DIR__ . '/foodslider.php';
     
-    $items = [
-        [
-            'id' => 1,
-            'name' => 'Pizza Margharita',
-            'ingredients' => 'Pizza with tomato souce, mozzarella fiordilatte & basilic powder',
-            'image'=> 'pizza.jpg',
-            'Category'=> 'Pizza',
-            'price' => 10.99,
-        ],
-        [
-            'id' => 2,
-            'name' => 'Chicken Burger',
-            'ingredients' => 'Pizza with tomato souce, mozzarella fiordilatte & basilic powder',
-            'image'=> 'burger.jpg',
-            'Category'=> 'Burgers',
-            'price' => 13.99,
-        ],
-        [
-            'id' => 3,
-            'name' => 'Maki',
-            'ingredients' => 'Pizza with tomato souce, mozzarella fiordilatte & basilic powder',
-            'image'=> 'sushi.jpg',
-            'Category'=> 'Sushi',
-            'price' => 7.45,
-        ],
-        [
-            'id' => 4,
-            'name' => 'Paella Valenciana',
-            'ingredients' => 'Pizza with tomato souce, mozzarella fiordilatte & basilic powder',
-            'image'=> 'spanish.jpg',
-            'Category'=> 'Spanish',
-            'price' => 15.69,
-        ]
-        ];
-
-    /*
+    
     if(isset($_GET['id'])){
         $categoryId = $db-> quote($_GET['id']);
 
-        $result = $db-> select("SELECT i.*,c.name as type FROM item i inner join category c on i.type=c.id WHERE i.id=". $categoryId);
+        $result = $db-> query("SELECT * FROM foods WHERE $categoryId=categories_ID");
 
-        if(count($result) > 0){
-            $items = [
-                'id'                => $result[0]['id'],
-                'category'          => $result[0]['category'],
-                'image'             => $result[0]['image'],
-                'name'              => $result[0]['name'],
-                'ingredientes'      => $result[0]['ingredients'],
-                'price'             => $result[0]['price'],
-            ];
-            echo $twig->render('item.html', ['item' => $items, 'categories' => $categories]);
+        if($result &&  $result->num_rows > 0){
+            $categoryId = trim($categoryId,"'");
+            echo $twig->render('item.html', ['navbar_data' => $navbar_data, 'categories' => $categories, 'selectedCategories' => [$categoryId], 'menuItems' => $result]);
         }else{
             echo $twig->render('404.html');
         }
     }else{
         echo $twig->render('404.html');
     }
-    */
-    echo $twig->render('item.html', ['navbar_data' => $navbar_data, 'categories' => $categories, 'selectedCategories' => [1], 'menuItems' => $items]);
-
-?>
