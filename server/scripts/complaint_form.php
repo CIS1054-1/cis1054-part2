d4d8fd1a9f06049f44f48eb852685859d304f555
carlosaalvrz <166819064+carlosaalvrz@users.noreply.github.com>
2024-05-14 19:01:35 +0200

    Complaint form to save into the database

---
+++ server/scripts/complaint_form.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Complaint form
+ *
+ * @author Carlos Alvarez
+ * @version 1.0
+ * @since 2023-05-14
+ */
+require_once __DIR__ . '/../autoload.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Retrieve the data from the form
+    $complaint_id = $db->quote($_SESSION['ID']); 
+    $subject = $db->quote($_POST['subject']);
+    $email = $db->quote($_POST['email']);
+    $name = $db->quote($_SESSION['name']);
+    $description = $db->quote($_SESSION['description']);
+    
+    if(!empty($complaint_id) && !empty($subject) && !empty($email) && !empty($name) && !empty($description)){
+
+        $insert_query = "INSERT INTO complaints (complaint_id, subject, email, name, description) VALUES ($complaint_id, $subject, $email, $name, $description )";
+        $db->query($insert_query);
+        header('Location: confirmation.php');
+        exit;
+    }else{
+        echo "ALARM 405: ACTIVE PROTECTION IP SERVER";
+    }
+    
+} else {
+    // If the request method is not POST, return an error response
+    echo "ALARM 405: ACTIVE PROTECION IP SERVED";
+}
+?>
